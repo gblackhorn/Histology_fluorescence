d63bbf57b847c9a76905d45cbd935bc5d748daef
gblackhorn <guoda.gd@gmail.com>
2022-09-08 17:55:21 +0900

    Added comments for readability

--- Workflow_average_fluorescence_intensity.m
+++ Workflow_average_fluorescence_intensity.m
@@ -1,49 +1,50 @@
-% Prerequirements (find details in "FIJI workflow for fluorescence intensity measurement" on Overleaf)
+% This workflow can load csv files containing FIJI measurement in chosen folder, group the data 
+% according to dilution, waiting-time, and ROI type, and finally plot the results  
+%
+% Pre-requirements (find details in "FIJI workflow for fluorescence intensity measurement" on Overleaf)
 % 	- Correct background 
 % 	- Correct shading (optional): no need to do this if the illumination is even 
-% 	- Measure flurecence intensity of ROIs in BG and Shading corrected image 
+% 	- Use FIJI/ImageJ to measure fluorescence intensity of ROIs in corrected image and save the measurements in csv files
 % 
-% Notes: Name all saved csv files with keywords representing the ROI types, such as 'BG', 'subnuclei', or'soma'
-
-
-% Contents
-%% ====================
-% 1. Default settings
-% clearvars -except recdata_organized alignedData_allTrials alignedData_event_list seriesData_sync grouped_event adata grouped_event_info_filtered
-
-% Folders
+% 
+% First of all: If you are using this code for the first time, modify some default settings below
+% - Modify the FIM_folder (fluorescence intensity measurement).
+%	FIM_folder will be the default location for loading csv files and save data. You will start to navigate from here in GUI
 PC_name = getenv('COMPUTERNAME'); 
-% set folders for different situation
-% DataFolder = 'G:\Workspace\Inscopix_Seagate';
-
-% FIM: fluorescence intensity measurement
-if strcmp(PC_name, 'GD-AW-OFFICE')
-	FIM_folder = 'G:\Workspace\confocal'; % office desktop
-elseif strcmp(PC_name, 'LAPTOP-84IERS3H')
-	FIM_folder = 'C:\Users\guoda\Documents\Workspace\Analysis\Confocal'; % laptop
+% Change the PC_name in the following lines, such as 'GD-AW-OFFICE' or 'LAPTOP-84IERS3H' to your own computer's name, and specify your FIM_folder
+if strcmp(PC_name, 'GD-AW-OFFICE') % GD's office desktop
+	FIM_folder = 'F:\Workspace\Confocal'; 
+elseif strcmp(PC_name, 'LAPTOP-84IERS3H') % GD's laptop
+	FIM_folder = 'C:\Users\guoda\Documents\Workspace\Analysis\Confocal'; 
 end
+%
+%
+% - Modify keywords: Keywords are used in csv file names. They represent dilution group, waiting time, and ROI types
+% Example csv file name: ****_D1_07_soma-1_**** (dilution1, 7 waiting days, soma ROIs, first csv file in this group)
+DilutionGroup = {'D1','D2','D3'}; % Dilution1, Dilution2, etc. 
+DayGroup = [7 14 21]; % Waiting time in days
+RegionGroup = {'subnuclei','soma'}; % ROI type
+
+
+%% ====================
+% 1. Prepare to analysis
+
+% Assign default folders
 DefaultDir.csv = FIM_folder;
 DefaultDir.mat = FIM_folder;
-% [FolderPathVA] = set_folder_path_ventral_approach(DataFolder,FIM_folder);
+DefaultDir.fig = FIM_folder;
 
-
-% Keywords and labels. 
-% Note: Name the CSV files using the labels below to group data automatically
-DilutionGroup = {'D1','D2','D3'}; 
-% DayGroup = {'7','14','21'};
-DayGroup = [7 14 21];
-RegionGroup = {'subnuclei','soma'};
-
+% Get the number of keywords in each category
 Di_num = numel(DilutionGroup);
 D_num = numel(DayGroup);
 R_num = numel(RegionGroup);
 
+% Group keywords 
 group_num = Di_num*D_num*R_num;
 keywords_groups = cell(1,group_num); % each cell contains a single string, such as 'D1-07-soma'
-keywords_cells = cell(1,group_num); % each cell contains a cell of keywords, such as {'D1','14','soma'}
+% keywords_cells = cell(1,group_num); % each cell contains a cell of keywords, such as {'D1','14','soma'}
 combined_csv_data = cell(1,group_num);
 kgc = 1; % current keyword group counting
-
 group_num_MonoRegion = Di_num*D_num;
 keywords_groups_MonoRegion = cell(1,group_num_MonoRegion);
 kgc_mr = 1;
@@ -54,11 +55,6 @@ for din = 1:Di_num
 		DStr = num2str(Dnum,'%02.f');
 		keywords_groups_MonoRegion{kgc_mr} = sprintf('%s-%s', DiStr,DStr); % example: 'D1-07'
 		for rn = 1:R_num
-            % fprintf('din-%d, dn-%d, rn-%d \n', din, dn, rn)
-            % if din == 3 && dn == 3 && rn == 1
-            % 	pause
-            % end
-            
 			RStr = RegionGroup{rn}; % example: 'soma' or 'nuclei'
 			keywords_groups{kgc} = sprintf('%s-%s-%s',DiStr,DStr,RStr);
 			keywords_cells_1{kgc} = {DiStr,DStr,RStr};
@@ -72,44 +68,33 @@ end
 
 %% ====================
 % 2. Collect csv files and combine all data into a single structure 
-% {'subnuclei','soma','D1','D2','D3','7','14','21'}
-% keywords_region{1} = {'subnuclei'}; % The first element in the cell will be used to mark the combined data
-% keywords_region{2} = {'soma'}; % The first element in the cell will be used to mark the combined data
 
-% Group csv data according to keywords below
-
+% Select the folder to load csv files with GUI
 csv_folder = uigetdir(DefaultDir.csv, 'Select a folder to read csv files in it');
 if csv_folder ~= 0
 	DefaultDir.csv = csv_folder;
 end
 
-% combined_csv_data = cell(size(keywords_region));
-% RegionType_num = numel(keywords_region);
+% Load csv files and save the data in a struct var
 for gn = 1:group_num
     keywords_1 = keywords_cells_1{gn};
     keywords_2 = keywords_cells_2{gn};
-    % fprintf('group_num: %d \n',gn);
 
 	[combined_csv_data{gn},csv_num,csv_list] = read_csv_files_in_folder(csv_folder,'keywords',keywords_2,...
-		'gui_read',false);
+		'del_col',{'StdDev'},'gui_read',false,'debug_mode',true);
 	if csv_num ~= 0
 		combined_csv_data{gn}.label = keywords_groups{gn};
 		combined_csv_data{gn}.dilution = keywords_1{1};
 		combined_csv_data{gn}.days = str2double(keywords_1{2});
 		combined_csv_data{gn}.region = keywords_1{3};
-
-		% keywords_WithSpace = cellfun(@(x) [x, ' '], keywords, 'UniformOutput',false); % add space to the end of each keyword
-		% keywords_string = string(keywords_WithSpace); % convert keywords cell array to string array for display
-		% combined_csv_data{gn}.RegionType = sprintf('%s',keywords_string); % add keywords as label
 	end
 end 
 roi_val_data = [combined_csv_data{:}]; % Put csv_data containing different region-type data into a single struct  
 
 %% ====================
-% 3. Save the roi_data
+% 3. Save the roi_val_data with GUI
 default_mat_path = fullfile(DefaultDir.mat,'*.mat');
 mat_folder = uigetdir(DefaultDir.mat,'Select a folder to save fluorescence intensity measurement');
-% [mat_file,mat_folder] = uiputfile(default_mat_path,'Save fluorescence intensity measurement', 'FIM.mat');
 if mat_folder ~= 0
 	DefaultDir.mat = mat_folder;
 	dt = datestr(now, 'yyyymmdd-HHMM');
@@ -147,29 +132,20 @@ for rn = 1:R_num
 			grouped_data(rn).data(gnmr).SteVal = std([FijiTbl.Mean]) / sqrt(length([FijiTbl.Mean]));
 		else
 			grouped_data(rn).data(gnmr).dilutionID = '';
-			% grouped_data(rn).data(gnmr).days = '';
 			grouped_data(rn).data(gnmr).days = [];
-
-			% grouped_data(rn).data(gnmr).RawVal = [FijiTbl.Mean];
-			% grouped_data(rn).data(gnmr).MeanVal = 0;
-			% grouped_data(rn).data(gnmr).StdVal = 0;
-			% grouped_data(rn).data(gnmr).SteVal = 0;
 		end
 	end 
-	% field_dilution = {grouped_data(rn).data.dilutionID};
-	% field_days = {grouped_data(rn).data.days};
-	% field_dilution = cellfun(@(x) char(x),field_dilution,'UniformOutput',false);
-	% field_days = cellfun(@(x) char(x),field_days,'UniformOutput',false);
-	% [grouped_data(rn).data.dilutionID] = field_dilution{:};
-	% [grouped_data(rn).data.days] = field_days{:};
 end
 
 %% ====================
 % 5. Plot data
 close all
+plot_region = 'soma'; % 'subnuclei' or 'soma'. Specify the ROI type
+
+% Settings for figures
+SaveFig = false; % true/false
 FontSize = 18;
 FontWeight = 'bold';
-plot_region = 'subnuclei'; % 'subnuclei' or 'soma'
 LineColors = {'#3FF5E6', '#F55E58', '#F5A427', '#4CA9F5', '#33F577',...
 		'#408F87', '#8F4F7A', '#798F7D', '#8F7832', '#28398F', '#000000'};
 LineWidth = 1;
@@ -221,3 +197,24 @@ legend('boxoff')
 set(gca,'box','off')
 set(gca,'FontSize',FontSize)
 set(gca,'FontWeight',FontWeight)
+
+
+if SaveFig
+	% default_fig_path = fullfile(DefaultDir.fig,'*.mat');
+	FigFolder = uigetdir(DefaultDir.fig,'Select a folder to save figures');
+	% [mat_file,mat_folder] = uiputfile(default_mat_path,'Save fluorescence intensity measurement', 'FIM.mat');
+	if mat_folder ~= 0
+		DefaultDir.fig = FigFolder;
+		dt = datestr(now, 'yyyymmdd-HHMM');
+		fbox_name = sprintf('%s_%s_boxplot',dt,plot_region);
+		savePlot(f_box,...
+			'guiSave', 'off', 'save_dir', FigFolder, 'fname', fbox_name);
+
+		fline_name = sprintf('%s_%s_lineplot',dt,plot_region);
+		savePlot(f_line,...
+			'guiSave', 'off', 'save_dir', FigFolder, 'fname', fline_name);
+
+		save(fullfile(FigFolder, [dt, '_boxplot_stat']),...
+		    'box_stat');
+	end
+end
